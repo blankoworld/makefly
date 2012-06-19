5c83665174e16fb8591ec4c2419ba15373936197
Olivier DOSSMANN <olivier+git@dossmann.net>
2012-06-19 11:26:37 +0200

    [ADD] Installation script for user that have a public_html directory

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+# install.sh
+
+# Copy pub directory to users' public_html
+
+SRCDIR=./pub
+DESTDIR=${HOME}/public_html
+
+process() {
+  rm -f ${DESTDIR}/* && cp ${SRCDIR}/* ${DESTDIR} && echo "...installed!"
+}
+
+echo "INSTALL to ${DESTDIR}..."
+
+if ! test -d ${SRCDIR}
+then
+  echo "${SRCDIR} directory not found!"
+  exit 1
+fi
+
+if ! test -d ${DESTDIR}
+then
+  echo "${DESTDIR} directory not found!"
+fi
+
+echo "This will delete ${DESTDIR} content and copy ${SRCDIR} into. Are you sure [y/n]?"
+read response
+if [ "${response}" = "y" ]
+then
+  process
+  exit 0
+elif [ "${response}" = "Y" ]
+then
+  process
+  exit 0
+else
+  exit 1
+fi
