b59aa6da50e5c1f2bf86d0c2278602426033840f
Olivier DOSSMANN <blankoworld@wanadoo.fr>
2012-07-17 17:38:35 +0200

    [ADD] Apply AGPL (GNU Affero General Public License)

--- bin/parser.lua
+++ bin/parser.lua
@@ -1,9 +1,33 @@
 #!/usr/bin/env lua
 
---Pour utiliser : ./truc variable1=valeur1 variable2=valeur2 <entrée >sortie
---permet de remplacer ${variable1} par "valeur1" dans une chaîne de caractère
---peut aussi s'utiliser ainsi : % lua test.lua "un cochon=un chat" <<<'Oh ! ${un cochon} !' 
---Oh ! un chat !
+--[[ LICENSE
+
+Makefly, a static weblog engine using a BSD Makefile
+Copyright (C) 2012 DOSSMANN Olivier
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+--]]
+
+--[[ USE
+
+lua parser.lua var1=value1 var2=value2 <in >out
+This permits to replace ${var1} by "value1" in a string.
+
+Could be used as this: % lua test.lua "un cochon=un chat" <<<'Oh ! ${un cochon} !' 
+Result: Oh ! un chat !
+--]]
 
 function replace(file, table)
   local s = file:read("*a")
