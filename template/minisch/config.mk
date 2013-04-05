05fb1b3cce7b65660cb51cabe99fcccf3694f448
Olivier DOSSMANN <olivier+git@dossmann.net>
2013-04-05 11:30:55 +0200

    [IMP] Update cascading stylesheet for all themes in order to simplify recognition in scripts

--- template/minisch/config.mk
+++ template/minisch/config.mk
@@ -1,6 +1,6 @@
 CSS_NAME = Minisch 2.4
-CSS_FILE = minisch.css
-CSS_COLOR_FILE = minisch_normal.css
+CSS_FILE = main_minisch.css
+CSS_COLOR_FILE = color_minisch_light.css
 JSKOMMENT_CAPTCHA_THEME = clean
 JSKOMMENT_CSS = jskomment.css
 SIDEBAR = 1 # force theme to have a sidebar (this is because it only works with sidebar)
--- template/responsive/config.mk
+++ template/responsive/config.mk
@@ -1,5 +1,5 @@
 CSS_NAME = Responsive
 CSS_FILE = main.css
-CSS_COLOR_FILE = responsive_blue.css
+CSS_COLOR_FILE = color_responsive_blue.css
 JSKOMMENT_CAPTCHA_THEME = white
 JSKOMMENT_CSS = jskomment.css
