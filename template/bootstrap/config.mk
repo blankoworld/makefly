83f79e238e91552ff6e7857db7c3fe3166e3ec6f
Olivier DOSSMANN <olivier+git@dossmann.net>
2013-04-30 11:38:17 +0200

    [IMP] Adapt bootstrap theme to Makefly's official weblog

--- template/bootstrap/config.mk
+++ template/bootstrap/config.mk
@@ -3,5 +3,5 @@ CSS_FILE = main_bootstrap.css
 CSS_COLOR_FILE ?= color_bootstrap_light.css
 JSKOMMENT_CAPTCHA_THEME = white
 JSKOMMENT_CSS = jskomment.css
-SIDEBAR = 1
+SIDEBAR = 0
 jskom_cont = ${THEMEDIR}/jskomment.article.xhtml
