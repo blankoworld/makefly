fc186d1f14fd9e9081bc4f0c910a21b6ea62403e
Olivier DOSSMANN <olivier+git@dossmann.net>
2013-07-26 16:38:18 +0200

    [FIX] Delete a useless tag

--- db/1341352800,official_weblog_open.mk
+++ db/1341352800,official_weblog_open.mk
@@ -1,6 +1,6 @@
 TITLE = Official weblog: Open
 DESCRIPTION = Makefly is now available on the web
-TAGS = news, a test to see if space breaks tags, web
+TAGS = news, web
 TYPE = special
 AUTHOR = olivier
 JSKOMMENT_PREFIX = 
