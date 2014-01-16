bd84731e7e9ea2032bb6dce4681ed02e823c7600
Olivier DOSSMANN <olivier+git@dossmann.net>
2014-01-16 21:08:59 +0100

    [FIX] fixes #196 - Add LC_ALL variable in create_post.sh script to avoid problem of non catched accentued chars

--- tools/create_post.sh
+++ tools/create_post.sh
@@ -80,6 +80,7 @@ read -p "Keywords: " keywords
 timestamp=`date +'%s'`
 
 # code retrived from Nanoblogger translit_text method with a little improvement for double "_"
+LC_ALL='C' # set local to C in order to take count the accentued chars on french environment
 nonascii="${title//[a-zA-Z0-9_-]/}" # isolate all non-printable/non-ascii characters
 new_name=$(echo "${title:0:$LIMIT}" |sed -e "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/; s/[\`\~\!\@\#\$\%\^\*\(\)\+\=\{\}\|\\\;\:\'\"\,\<\>\/\?]//g; s/ [\&] / and /g; s/^[ ]//g; s/[ ]$//g; s/[\.]/_/g; s/\[//g; s/\]//g; s/ /_/g; s/[$nonascii ]/_/g" |sed -e '/[\_\-]*$/ s///g; /[\_\-]$/ s///g' |sed -e 's/__/_/g')
 
