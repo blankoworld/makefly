7deea4456f459e05a39d64748d4525bbd240892c
Olivier DOSSMANN <olivier+git@dossmann.net>
2014-03-31 14:02:38 +0200

    [ADD] New article: Serveur VirtualBox

---
+++ db/1396265170,serveur_de_machines_virtuelles_avec_virtualbox.mk
@@ -0,0 +1,6 @@
+TITLE = Serveur de machines virtuelles avec VirtualBox
+DESCRIPTION = Comment utiliser VirtualBox sans environnement graphique sur le serveur distant
+TAGS = astuce, libre, logiciels, web
+TYPE = normal
+AUTHOR = olivier
+KEYWORDS = virtualbox, machine, virtuelle, debian, remotebox, console, ligne de commande
