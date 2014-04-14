26cd35af24001ccd6d6cefc1d8910ee7b3fc66ab
Olivier DOSSMANN <olivier+git@dossmann.net>
2014-04-14 19:57:24 +0200

    [ADD] New post about a dokuwiki script for multiple installations

---
+++ db/1397494494,installations_multiples_de_dokuwiki_script_de_mise_jour.mk
@@ -0,0 +1,6 @@
+TITLE = Installations multiples de Dokuwiki, script de mise à jour
+DESCRIPTION = Comment mettre à jour de multiples installations de Dokuwiki en un rien de temps
+TAGS = developpement, logiciels, libre, web
+TYPE = normal
+AUTHOR = olivier
+KEYWORDS = dokuwiki, installation, multiple, script, mise à jour
