e6311644b668c6b580cfc6ecedbf792d557584d0
Olivier DOSSMANN <olivier+git@dossmann.net>
2015-10-19 17:59:56 +0200

    [FIX] #356 - Documentation: Add part of AGPL license on all files

--- lib/blog.lua
+++ lib/blog.lua
@@ -1,3 +1,25 @@
+-------------------------------------------------------------------------------
+-- LICENSE
+--
+-- Makefly, a static weblog engine using Lua
+-- Copyright (C) 2012-2015 DOSSMANN Olivier, <olivier+makefly@dossmann.net>
+--
+-- This file is part of Makefly.
+-- 
+-- Makefly is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- Makefly is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+-- 
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-------------------------------------------------------------------------------
+
 local blog = { }
 
 local utils = require 'lib.utils'
